54aa448c99687814a21f433d93d5e557cea11731
j0sky <grishko.e@gmail.com>
2016-09-22 18:05:57 +1000

    - Мелкие исправления - Броня окрашивается и вставляется в слот инвентаря - Выводится сообщение о выдаче стартового комплекта

--- src/main/java/rmcpe/Kits/Kits.java
+++ src/main/java/rmcpe/Kits/Kits.java
@@ -1,25 +1,28 @@
 package rmcpe.Kits;
 
-import cn.nukkit.OfflinePlayer;
 import cn.nukkit.Player;
-import cn.nukkit.Server;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.inventory.PlayerInventory;
 import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemColorArmor;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
 import cn.nukkit.utils.TextFormat;
 
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 /**
- * Created by user11 on 16.09.2016.
+ * Created by j0sky on 16.09.2016.
  */
 public class Kits extends PluginBase implements Listener {
 
-    ArrayList itemsList = new ArrayList();
+    private ArrayList itemsList = new ArrayList();
 
     @Override
     public void onEnable(){
@@ -31,7 +34,7 @@ public class Kits extends PluginBase implements Listener {
 
     }
 
-    public void initConfig(){
+    private void initConfig() {
         this.getDataFolder().mkdirs();
         this.saveResource("config.yml");
     }
@@ -51,10 +54,51 @@ public class Kits extends PluginBase implements Listener {
 
             while (var4.hasNext()) {
                 Item item = (Item) var4.next();
-                if(player.getInventory().canAddItem(item)) {
-                    player.getInventory().addItem(new Item[]{item});
+                PlayerInventory inv = player.getInventory();
+                if (inv.canAddItem(item)) {
+                    if (item.isArmor()) {
+//                        TODO: Цвет, названия - перенести в конфиг. Сделать опциональным.
+                        if (item.isBoots()) {
+                            ItemColorArmor colorizeditem = (ItemColorArmor) item;
+                            colorizeditem.setColor(50, 50, 255);
+                            colorizeditem.setCustomName("Ботинки новичка");
+                            inv.setBoots(colorizeditem);
+                        } else if (item.isChestplate()) {
+                            ItemColorArmor colorizeditem = (ItemColorArmor) item;
+                            colorizeditem.setColor(50, 50, 255);
+                            item.setCustomName("Куртка новичка");
+                            inv.setChestplate(colorizeditem);
+                        } else if (item.isHelmet()) {
+                            ItemColorArmor colorizeditem = (ItemColorArmor) item;
+                            colorizeditem.setColor(50, 50, 255);
+                            item.setCustomName("Шапка новичка");
+                            inv.setHelmet(colorizeditem);
+                        } else if (item.isLeggings()) {
+                            ItemColorArmor colorizeditem = (ItemColorArmor) item;
+                            colorizeditem.setColor(50, 50, 255);
+                            item.setCustomName("Штаны новичка");
+                            inv.setLeggings(colorizeditem);
+                        }
+
+                    }
+                    //TODO: Добавить предметы в хотбар.
+                    /*else if (item.isSword()) {
+
+                    }*/
+                    else {
+                        inv.addItem(new Item[]{item});
+                    }
                 }
             }
+
+            player.sendMessage("" + TextFormat.ITALIC + TextFormat.AQUA + "Вам был выдан кит Старт. Загляните в инвентарь ;)");
+
+//            Map<Integer, Item> contents = player.getInventory().getContents();
+//            for (Map.Entry<Integer, Item> entry : contents.entrySet())
+//            {
+//                System.out.println(entry.getKey() + "/" + entry.getValue());
+//                this.getLogger().info("§d item: " +Item.get(entry.getValue().getId()).getName() );
+//            }
         }
 
     }
@@ -74,10 +118,8 @@ public class Kits extends PluginBase implements Listener {
             while(var2.hasNext()) {
                 String coords = (String)var2.next();
                 String[] str = coords.split(",");
-                Item item = new Item(Integer.parseInt(str[0]), Integer.parseInt(str[1]), Integer.parseInt(str[2]));
+                Item item = Item.get(Integer.parseInt(str[0]), Integer.parseInt(str[1]), Integer.parseInt(str[2]));
                 this.itemsList.add(item);
-                this.getLogger().info("§a var2: " +var2 );
-                this.getLogger().info("§d item: " +item );
             }
         }
         return itemsList;
